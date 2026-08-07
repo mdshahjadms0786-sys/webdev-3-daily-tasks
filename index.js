2e34f15b6ca34effe93f05d1a9ac1ee64b964aaa
Md Shahjad <mdshahjadms0786@gmail.com>
2026-08-07 12:08:13 +0530

    Day 2: updated files

--- index.js
+++ index.js
@@ -4,3 +4,15 @@ require("./data/data");
 const path = require("path");
 const filePath = path.join(__dirname, "data", "data.txt");
 console.log(filePath);
+
+
+// const path = require('path')
+// const filePath = path.join(__dirname, 'data', 'data.txt')
+// console.log(filePath)
+
+// const process = require('process')
+// console.log(process.argv)
+// console.log(process.argv[2])
+
+require('dotenv').config()
+console.log(process.env.PORT)
