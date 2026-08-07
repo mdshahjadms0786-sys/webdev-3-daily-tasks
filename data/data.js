3ed4dfe5790c082f816f6f1f0b1e3a5aa7929115
Md Shahjad <mdshahjadms0786@gmail.com>
2026-08-07 11:51:42 +0530

    Day 2: day 2 topic coverd

---
+++ data/data.js
@@ -0,0 +1,26 @@
+// const cities = ["goa", "delhi", "mumbai", "bangalore", "chennai", "kolkata", "hyderabad", "pune", "jaipur", "lucknow"];
+
+// module.exports = cities;
+
+// console.log(cities);
+
+// const chalk = require("chalk");
+// console.log(chalk.blue("Hello world!"));
+
+// import chalk from "chalk";
+// console.log(chalk.blue("Hello world!"));
+
+
+// const os = require("os");
+// const userInfo = os.userInfo();
+// const platform = os.platform();
+// const architecture = os.arch();
+// const uptime = os.uptime();
+// const totalMemory = os.totalmem();
+// const freeMemory = os.freemem();
+// console.log(`User Info: ${JSON.stringify(userInfo)}`);
+// console.log(`Platform: ${platform}`);
+// console.log(`Architecture: ${architecture}`);
+// console.log(`Uptime: ${uptime}`);
+// console.log(`Total Memory: ${totalMemory}`);
+// console.log(`Free Memory: ${freeMemory}`);
---
+++ index.js
@@ -0,0 +1,6 @@
+require("./data/data");
+
+
+const path = require("path");
+const filePath = path.join(__dirname, "data", "data.txt");
+console.log(filePath);
