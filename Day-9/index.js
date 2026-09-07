a44a17d4e7e92d0e5e51c750facc8130881869be
mdshahjadms0786@gmail.com <mdshahjadms0786@gmail.com>
2026-09-07 12:19:26 +0530

    WebDev-III Daily Tasks Day-1 to Day-9

---
+++ Day-9/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+const morgan = require('morgan');
+
+// use morgan middleware for logging
+app.use(morgan('dev'));
+
+// built-in middleware to handle JSON data
+app.use(express.json());
+
+// built-in middleware to handle urlencoded form data
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    console.log('Request Method:', req.method);
+    console.log('Request URL:', req.url);
+    console.log('Request Headers:', req.headers);
+  res.send('Home Page');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
